aebe4be09b13ea2e249da46c4b1146d407358b8f
KaimuSeino <skim07a19yki@icloud.com>
2024-07-11 09:23:20 +0900

    条件からリスト処理を工夫した

---
+++ atcoder_problems/C/132.py
@@ -0,0 +1,11 @@
+N = int(input()) # Nは偶数
+d_list = list(map(int, input().split()))
+
+# リストを小さい順にする
+d_list.sort(reverse=False)
+
+# リストを半分に分割した時の N / 2と N / 2 -1の値が同じである場合
+if d_list[int(N / 2)] == d_list[int(N / 2 - 1)]:
+    print(0)
+else:
+    print(d_list[int(N / 2)] - d_list[int(N / 2 - 1)])
