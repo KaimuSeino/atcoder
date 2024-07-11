3ee2f43d906b78a3ad3dff7923da9f8583e61002
KaimuSeino <skim07a19yki@icloud.com>
2024-07-11 09:22:39 +0900

    コメントと復習

--- atcoder_problems/B/067.py
+++ atcoder_problems/B/067.py
@@ -1,10 +1,13 @@
 N, K = map(int, input().split())
 L_list = list(map(int, input().split()))
 
+# 大きい順に並べる
 L_list.sort(reverse=True)
 
+# 長さを保存する
 length = 0
 
+# 長い順の棒を三つ足す
 for i in range(K):
     length += L_list[i]
 
